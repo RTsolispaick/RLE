a8528cd78640bc24fca0e091a7e71ac4d9c4f031
Dr. Livesey <42037004+RTsolispaick@users.noreply.github.com>
2022-10-17 00:14:57 +0500

    Update vm.js

--- js/vm/vm.js
+++ js/vm/vm.js
@@ -5,42 +5,42 @@ let power = 1;
 let code = fs.readFileSync(process.argv[2], "utf-8");
 code = code.replace(/\n/g, " ");
 let memory = new Array();
-let ep = 0;
+let ip = 0;
 
 memory = code.split(" ");
 
 while (power) {
-    switch (memory[ep]) {
+    switch (memory[ip]) {
         case "set":
-            memory[1 * memory[ep + 1]] = 1 * memory[ep + 2];
-            ep += 3;
+            memory[1 * memory[ip + 1]] = 1 * memory[ip + 2];
+            ip += 3;
             break;
         case "jsin":
             let word = readline.question();
-            memory[1 * memory[ep + 1]] = word;
-            ep += 2;
+            memory[1 * memory[ip + 1]] = word;
+            ip += 2;
             break;
         case "jsout":
-            console.log(memory[1 * memory[ep + 1]]);
-            ep += 2;
+            console.log(memory[1 * memory[ip + 1]]);
+            ip += 2;
             break;
         case "add":
-            memory[1 * memory[ep + 3]] = 1 * memory[1 * memory[ep + 1]] + 1 * memory[1 * memory[ep + 2]];
-            ep += 4;
+            memory[1 * memory[ip + 3]] = 1 * memory[1 * memory[ip + 1]] + 1 * memory[1 * memory[ip + 2]];
+            ip += 4;
             break;
         case "mul":
-            memory[1 * memory[ep + 3]] = (1 * memory[1 * memory[ep + 1]]) * (1 * memory[1 * memory[ep + 2]]);
-            ep += 4;
+            memory[1 * memory[ip + 3]] = (1 * memory[1 * memory[ip + 1]]) * (1 * memory[1 * memory[ip + 2]]);
+            ip += 4;
             break;
         case "jmp":
-            ep = 1 * memory[ep + 1];
+            ip = 1 * memory[ip + 1];
             break;
         case "jscmp":
-            if (1 * memory[1 * memory[ep + 1]] < 1 * memory[1 * memory[ep + 2]]) {
-                ep += 4;
+            if (1 * memory[1 * memory[ip + 1]] < 1 * memory[1 * memory[ip + 2]]) {
+                ip += 4;
                 break;
             }
-            ep = 1 * memory[ep + 3];
+            ip = 1 * memory[ip + 3];
             break;
         default:
             console.log("Finish");
