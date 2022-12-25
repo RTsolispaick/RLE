ffd8eb9921a2027c00092e4a6f9e2b61711743eb
Dr. Livesey <42037004+RTsolispaick@users.noreply.github.com>
2022-12-25 23:31:52 +0500

    Update BooerMoor.js

--- js/BooerMoor/BooerMoor.js
+++ js/BooerMoor/BooerMoor.js
@@ -4,16 +4,39 @@ const fs = require('fs');
 let string = fs.readFileSync('str.txt', 'utf8');
 //kolokol
 let pattern = fs.readFileSync('pattern.txt', 'utf8');
-let vhoh = new Array();
 
+let m = pattern.length;
+let vhoh = new Array();
+let rpr = new Array();
+let Shift2 = new Array();
+let Shift1;
+
+//Bad suffix N(char)
 let N = new Array()
 for (let j = 0; j < pattern.length; j++)
     N[pattern.charAt(j)] = j;
 
-let Shift1;
+//Good suffix
+let patternMore = '*'.repeat(m) + pattern;
+
+for (let i = 0; i <= pattern.length; i++) {
+    rpr[i] = m;
+    k = m - i;
+    while (true) {
+        if (compare(patternMore.substring(k + m - 1, k + m + i - 1), pattern.substring(m - i, m))
+            && (k > 1 && pattern[k - 2] != pattern[m - i - 1] || k <= 1)) break;
+        k--;
+    }
+    rpr[i] = k;
+}
+
+for (let i = 0; i <= pattern.length; i++) {
+    Shift2[i] = m - rpr[i] - i;
+}
+
+//Search
 let j = 0;
 for (let i = pattern.length - 1; i <= string.length - 1;) {
-    console.log(i);
     while (string[i - j] == pattern[pattern.length - 1 - j]) {
         if (j == pattern.length - 1) {
             vhoh.push(i - pattern.length + 1);
@@ -23,13 +46,26 @@ for (let i = pattern.length - 1; i <= string.length - 1;) {
     }
     if (!N[string[i - j]]) N[string[i - j]] = 0;
     Shift1 = Math.max(pattern.length - N[string[i - j]] - j - 1, 1);
-    i += Shift1;
+    S = Math.max(Shift1, Shift2[j]);
+    i += S;
     j = 0;
 }
 
+//Output
 for (i in vhoh) {
     let str = "Index of occurrence: " + vhoh[i] + '\n';
     fs.writeFileSync('index.txt',
         str,
         { encoding: "utf-8", flag: "a" });
+}
+
+//Compair T* and T
+function compare(t1, t2) {
+    for (let i = 0; i < t1.length; i++) {
+        if (t1[i] == '*' || t2[i] == '*' || t1[i] == t2[i]) {
+            continue;
+        }
+        return false;
+    }
+    return true;
 }
